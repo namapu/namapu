b3bd7a88b474571bd9edfa9b2c4fd4ce40529cec
namapu <namapu@namapu.com>
2023-02-26 18:57:05 +0100

    small updates

--- src/app.test.ts
+++ src/app.test.ts
@@ -2,7 +2,7 @@
 import { expect, test } from 'vitest';
 
 test('Math.sqrt()', () => {
-  expect(Math.sqrt(4)).toBe(2);
+  expect(3 * 3).toBe(9);
   expect(Math.sqrt(144)).toBe(12);
   expect(Math.sqrt(2)).toBe(Math.SQRT2);
 });
--- src/main.test.ts
+++ src/main.test.ts
@@ -2,7 +2,8 @@
 import { expect, test } from 'vitest';
 
 test('Math.sqrt()', () => {
-  expect(Math.sqrt(9)).toBe(3);
+  expect(Math.sqrt(16)).toBe(4);
   expect(Math.sqrt(144)).toBe(12);
   expect(Math.sqrt(2)).toBe(Math.SQRT2);
 });
+
