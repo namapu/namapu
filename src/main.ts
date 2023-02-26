d70c1f1d6a33335eb23f887df8d526bb5e48f1ce
namapu <namapu@namapu.com>
2023-02-26 20:02:32 +0100

    smaller font at nav

--- src/main.ts
+++ src/main.ts
@@ -13,26 +13,26 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 window.onload = () => {
   const appElem = document.querySelector<HTMLDivElement>('#app') as HTMLDivElement;
   appElem!.innerHTML = `
-  <div>
-    <br />
-    <h1>NAMAPU</h1>
-    <p>
-     Welcome to NAMAPU 
-    </p>
-  </div>
-`;
+    <div>
+      <br />
+      <h1>NAMAPU</h1>
+      <p>
+      Welcome to NAMAPU 
+      </p>
+    </div>
+    `;
 
   const navElem = document.querySelector<HTMLDivElement>('#nav') as HTMLDivElement;
   navElem!.innerHTML = `
-  <nav style="position:fixed; left: 0.5em; top:0.5em">
-    <button id='btn-new'  onclick="this.handleClick_New()">New</button>
-    <button id='btn-load' onclick="handleClick_Load()">Load</button>
-    <button id='btn-save' onclick="handleClick_Save()">Save</button>
-    <button id='btn-dele' onclick="handleClick_Delete()">Delete</button>
-    <button id='btn-undo' onclick="handleClick_Undo()">Undo</button>
-    <button id='btn-redo' onclick="handleClick_Redo()">Redo</button>
-  </nav>
-  `;
+    <nav style="position:fixed; left: 0.5em; top:0.5em">
+      <button id='btn-new'  onclick="this.handleClick_New()">New</button>
+      <button id='btn-load' onclick="handleClick_Load()">Load</button>
+      <button id='btn-save' onclick="handleClick_Save()">Save</button>
+      <button id='btn-dele' onclick="handleClick_Delete()">Delete</button>
+      <button id='btn-undo' onclick="handleClick_Undo()">Undo</button>
+      <button id='btn-redo' onclick="handleClick_Redo()">Redo</button>
+    </nav>
+    `;
 
 
   // let app = new App(appElem, navElem);
