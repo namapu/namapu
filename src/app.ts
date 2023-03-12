28d128730b478ffe90b362689f5b7d1918ded3e2
namapu <namapu@namapu.com>
2023-03-12 19:58:30 +0100

    fixed problem with document in main by moving the app to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,126 +1,101 @@
-// import CardItem from './card-item';
-import Nav from './nav';
-import { TAppState } from './types';
-// import { THistory } from './doc';
-// import Immutable from 'immutable';
+// import { v4 as uuidv4 } from 'uuid';
 
-export default class App {
-  readonly appElem: HTMLDivElement;
-  readonly navElem: HTMLDivElement;
-  readonly nav: Nav;
-  readonly appState: TAppState;
+export enum EType { ERR = 0, CARD, CONN, FRAME, SVG };
+export enum EOp { ERR = 0, ADD, REMOVE, CHANGE, CLEARALL, UNDO, REDO };
+export type TJsonString = string;
 
-  constructor(appState: TAppState, appElem: HTMLDivElement, navElem: HTMLDivElement) {
-    this.appState = appState;
-    this.appElem = appElem;
-    this.navElem = navElem;
-    this.nav = new Nav(this.appState, this.navElem);
-    this.bindEvents();
-  }
-/*
-  operation(fn: any) {
-    // first, make sure that there is no future
-    // in the history list. for instance, if the user
-    // renders something, clicks undo, and then
-    // renders something else, we need to dispose of the
-    // future state
-    const index = this.history.index;
-    this.history.mindmaps = this.history.mindmaps.slice(0, index + 1);
-    
-    // create a new version of the data by applying
-    // a given function to the current head
-    const newVersion = fn(this.history.mindmaps[index]);
-    
-    // add the new version to the history list and increment
-    // the index to match
-    this.history.mindmaps.push(newVersion);
-    this.history.index = index + 1;
-    
-    // rerender the cards
-    this.render();
-  }
- 
-  addCard(x: number, y: number, color: number, name: string, id: string) {
-    this.operation(function(data: any) {
-        return data.push(Immutable.Map({
-            x: x, y: y, color: color, name: name, id: id
-        }));
-    });
-  }
+export interface IItem {
+	typ: EType; id: string;
+};
+export interface ICard {
+	typ: EType; id: string; tit: string;
+	x: number; y: number; w: number; h: number;
+	col: string; bgcol: string;
+}
+export interface ICmd { op: EOp, item1: TJsonString, item2: TJsonString };
+export interface IApp {
+	state: Map<string, TJsonString>;
+	undo: Array<ICmd>;
+	redo: Array<ICmd>;
+};
 
-  removeCard(id: string) {
-    this.operation(function(data: any) {
-        return data.filter(function(card: any) {
-            return card.get('id') !== id;
-        });
-    });
-  }
-*/
-  // over_handler(event: PointerEvent) { console.log(event); }
-  // enter_handler(event: PointerEvent) { console.log(event); }
-  // down_handler(event: PointerEvent) { console.log(event); }
-  // move_handler(event: PointerEvent) { console.log(event); }
-  // up_handler(event: PointerEvent) { console.log(event); }
-  // cancel_handler(event: PointerEvent) { console.log(event); }
-  // out_handler(event: PointerEvent) { console.log(event); }
-  // leave_handler(event: PointerEvent) { console.log(event); }
-  // gotcapture_handler(event: PointerEvent) { console.log(event); }
-  // lostcapture_handler(event: PointerEvent) { console.log(event); }
-  // click_handler(event: MouseEvent) { console.log(event); alert("click"); }
-  // dblclick_handler(event: MouseEvent) { console.log(event); alert("dblclick"); }
-  
-  render = () => {
-    this.nav.render();
-    // this.doc.render();
-  }
-  
-  private bindEvents = () => {
-      // this.appElem.onclick = this.handleClick;
-      // this.appElem.ondblclick = this.handleClick;    // Is double click better?
-  }
-
-  // private handleClick = (event: MouseEvent) => {
-  //   if (event.target === this.appElem) {
-  //     this.handleClick_AddCard(event);
-  //   }
-  //   else {
-  //     this.handleClick_EditCard(event);
-  //   }
-  // }
-
-//   private handleClick_AddCard = (event: MouseEvent) => { 
-//     // const cardItem = this.doc.addCard(event.pageX, event.pageY);
-//     // this.doc.focusUid = cardItem.uid;
-//     // // console.log("focusUid: ", this.doc.focusUid);
-//     // this.render();
-//     // this.editCardTitle(cardItem);
-//   }
-
-// private editCardTitle(cardItem: CardItem) {
-//   const card = document.querySelector<HTMLElement>("#" + cardItem.uid) as HTMLElement;
-//   if (card != null) {
-//     card.contentEditable = 'true';
-//     card.focus();
-//     handleFocusOut(card, cardItem);   
-//   }
-// }
-
-// private handleClick_EditCard = (event: MouseEvent) => {
-//     console.log("card clicked: " + event);
-//     const item = event.target as HTMLElement
-//     this.doc.focusUid = item.id;
-//     console.log("focusUid: ", this.doc.focusUid);
-//   }
-// }
-
-// function handleFocusOut(card: HTMLElement, cardItem: CardItem) {
-//   card.addEventListener('focusout', (e: Event) => {
-//     const foc = e as FocusEvent;
-//     if (foc !== null && foc.target !== null) {
-//       const el: HTMLElement = foc.target as HTMLElement;
-//       cardItem.title = el.innerText;
-//       console.log(cardItem.title);
-//     }
-//   });
+function pushTo(arr: Array<ICmd>, item1: TJsonString, item2: TJsonString, op: EOp) {
+	const cmd: ICmd = { op: op, item1: item1, item2: item2 };
+	arr.push(cmd);
 }
 
+export function add(app: IApp, item: TJsonString) {
+	const json: IItem = JSON.parse(item as string) as IItem;
+	app.state.set(json.id, item);
+	pushTo(app.undo, item, "{}", EOp.REMOVE);
+	app.redo.length = 0;
+}
+
+export function remove(app: IApp, item: TJsonString) {
+	const json: IItem = JSON.parse(item as string) as IItem;
+	app.state.delete(json.id);
+	pushTo(app.undo, item, "{}", EOp.ADD);
+	app.redo.length = 0;
+}
+
+export function change(app: IApp, item1: TJsonString, item2: TJsonString) {
+	const json1: IItem = JSON.parse(item1 as string) as IItem;
+	app.state.set(json1.id, item2);
+	pushTo(app.undo, item1, item2, EOp.CHANGE);
+	app.redo.length = 0;
+}
+
+export function undo(app: IApp) {
+	const cmd: ICmd = app.undo.pop() as ICmd;
+	switch (cmd.op) {
+		case EOp.ADD: {
+			const json1: IItem = JSON.parse(cmd.item1 as string) as IItem;
+			app.state.set(json1.id, cmd.item1);
+			pushTo(app.redo, cmd.item1, "{}", EOp.REMOVE);
+			break;
+		}
+		case EOp.REMOVE: {
+			const json1: IItem = JSON.parse(cmd.item1 as string) as IItem;
+			app.state.delete(json1.id);
+			pushTo(app.redo, cmd.item1, "{}", EOp.ADD);
+			break;
+		}
+
+		case EOp.CHANGE: {
+			const json1: IItem = JSON.parse(cmd.item1 as string) as IItem;
+			app.state.set(json1.id, cmd.item1);
+			pushTo(app.redo, cmd.item2, cmd.item1, EOp.CHANGE);
+			break;
+		}
+
+		default:
+			throw "Unknown operation: " + cmd.op;
+	}
+};
+
+export function redo(app: IApp) {
+	const cmd: ICmd = app.redo.pop() as ICmd;
+	switch (cmd.op) {
+		case EOp.ADD: {
+			const json1: IItem = JSON.parse(cmd.item1 as string) as IItem;
+			app.state.set(json1.id, cmd.item1);
+			pushTo(app.undo, cmd.item1, "{}", EOp.REMOVE);
+			break;
+		}
+		case EOp.REMOVE: {
+			const json1: IItem = JSON.parse(cmd.item1 as string) as IItem;
+			app.state.set(json1.id, cmd.item1);
+			pushTo(app.undo, cmd.item1, "{}", EOp.ADD);
+			break;
+		}
+		case EOp.CHANGE: {
+			const json1: IItem = JSON.parse(cmd.item1 as string) as IItem;
+			app.state.set(json1.id, cmd.item1);
+			pushTo(app.undo, cmd.item2, cmd.item1, EOp.CHANGE);
+			break;
+		}
+
+		default:
+			throw "Unknown operation: " + cmd.op;
+	}
+};
--- src/main.ts
+++ src/main.ts
@@ -1,105 +1,6 @@
 import './style.css'
 import { v4 as uuidv4 } from 'uuid';
-
-export enum EType { ERR = 0, CARD, CONN, FRAME, SVG };
-export enum EOp { ERR = 0, ADD, REMOVE, CHANGE, CLEARALL, UNDO, REDO };
-export type TJsonString = string;
-
-export interface IItem {
-	typ: EType; id: string;
-};
-export interface ICard {
-	typ: EType; id: string; tit: string;
-	x: number; y: number; w: number; h: number;
-	col: string; bgcol: string;
-}
-export interface ICmd { op: EOp, item1: TJsonString, item2: TJsonString };
-export interface IApp {
-	state: Map<string, TJsonString>;
-	undo: Array<ICmd>;
-	redo: Array<ICmd>;
-};
-
-function pushTo(arr: Array<ICmd>, item1: TJsonString, item2: TJsonString, op: EOp) {
-	const cmd: ICmd = { op: op, item1: item1, item2: item2 };
-	arr.push(cmd);
-}
-
-export function add(app: IApp, item: TJsonString) {
-	const json: IItem = JSON.parse(item as string) as IItem;
-	app.state.set(json.id, item);
-	pushTo(app.undo, item, "{}", EOp.REMOVE);
-	app.redo.length = 0;
-}
-
-export function remove(app: IApp, item: TJsonString) {
-	const json: IItem = JSON.parse(item as string) as IItem;
-	app.state.delete(json.id);
-	pushTo(app.undo, item, "{}", EOp.ADD);
-	app.redo.length = 0;
-}
-
-export function change(app: IApp, item1: TJsonString, item2: TJsonString) {
-	const json1: IItem = JSON.parse(item1 as string) as IItem;
-	app.state.set(json1.id, item2);
-	pushTo(app.undo, item1, item2, EOp.CHANGE);
-	app.redo.length = 0;
-}
-
-export function undo(app: IApp) {
-	const cmd: ICmd = app.undo.pop() as ICmd;
-	switch (cmd.op) {
-		case EOp.ADD: {
-			const json1: IItem = JSON.parse(cmd.item1 as string) as IItem;
-			app.state.set(json1.id, cmd.item1);
-			pushTo(app.redo, cmd.item1, "{}", EOp.REMOVE);
-			break;
-		}
-		case EOp.REMOVE: {
-			const json1: IItem = JSON.parse(cmd.item1 as string) as IItem;
-			app.state.delete(json1.id);
-			pushTo(app.redo, cmd.item1, "{}", EOp.ADD);
-			break;
-		}
-
-		case EOp.CHANGE: {
-			const json1: IItem = JSON.parse(cmd.item1 as string) as IItem;
-			app.state.set(json1.id, cmd.item1);
-			pushTo(app.redo, cmd.item2, cmd.item1, EOp.CHANGE);
-			break;
-		}
-
-		default:
-			throw "Unknown operation: " + cmd.op;
-	}
-};
-
-export function redo(app: IApp) {
-	const cmd: ICmd = app.redo.pop() as ICmd;
-	switch (cmd.op) {
-		case EOp.ADD: {
-			const json1: IItem = JSON.parse(cmd.item1 as string) as IItem;
-			app.state.set(json1.id, cmd.item1);
-			pushTo(app.undo, cmd.item1, "{}", EOp.REMOVE);
-			break;
-		}
-		case EOp.REMOVE: {
-			const json1: IItem = JSON.parse(cmd.item1 as string) as IItem;
-			app.state.set(json1.id, cmd.item1);
-			pushTo(app.undo, cmd.item1, "{}", EOp.ADD);
-			break;
-		}
-		case EOp.CHANGE: {
-			const json1: IItem = JSON.parse(cmd.item1 as string) as IItem;
-			app.state.set(json1.id, cmd.item1);
-			pushTo(app.undo, cmd.item2, cmd.item1, EOp.CHANGE);
-			break;
-		}
-
-		default:
-			throw "Unknown operation: " + cmd.op;
-	}
-};
+import { IApp, TJsonString, ICard, ICmd, EType, add, remove, change, undo, redo } from './app';
 
 function render(app: IApp) {
 	let innerHTML = "";
@@ -110,8 +11,6 @@ function render(app: IApp) {
 	const mapDiv: HTMLDivElement = document.getElementById("map") as HTMLDivElement;
 	mapDiv.innerHTML = innerHTML;
 }
-
-
 
 
 function startTime() {
@@ -256,7 +155,7 @@ function runRenderBigTest() {
 	console.log("============== elapsed time: start ==============");
 	const N = 100;
 	for (let i = 0; i < N; i++) {
-		add(app, JSON.stringify({ typ: EType.CARD, id: uuidv4(), tit: "Hello, World", 
+		add(app, JSON.stringify({ typ: EType.CARD, id: uuidv4(), tit: "Card", 
 		x: rndInt(0, 1000), y: rndInt(0, 800), bgcol: rndInt(0, 360) }));
 	}
 
@@ -267,4 +166,4 @@ function runRenderBigTest() {
 }
 // runAddRemoveTest();
 // runRenderTest();
-// runRenderBigTest();
+runRenderBigTest();
