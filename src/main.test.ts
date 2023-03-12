0b9edf114f3f6ec2a7905b6839526d68d460191c
namapu <namapu@namapu.com>
2023-03-12 19:49:01 +0100

    added render of cards

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,8 +1,8 @@
 // example.test.js
 import { expect, test } from 'vitest';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 
-import { IApp, TJsonString, ICmd, EType, addItem, removeItem, undo, redo } from './main';
+import { IApp, TJsonString, ICmd, EType, add, remove, change, undo, redo } from './main';
 
 test('Math.sqrt()', () => {
 
@@ -27,16 +27,15 @@ test('Math.sqrt()', () => {
     const id0 = "6eac60fe-79f1-4f93-8566-3cbb348e0912"; //uuidv4();
     const id1 = "68ea219a-d092-431b-8393-b94fe8e2c889"; //uuidv4();
     const id2 = "836ca1f3-5956-41a8-9044-3f19444b5f3d"; //uuidv4();
-    const id3 = "e1f292fe-866c-42a0-a45f-d80f46e3c35f"; //uuidv4();
-    const id4 = "1f75218b-39c8-46e4-956b-edbed0235379"; //uuidv4();
+    const id3 = "1f75218b-39c8-46e4-956b-edbed0235379"; //uuidv4();
 
     expect(app.state.size).toBe(0);
     expect(app.undo.length).toBe(0);
     expect(app.redo.length).toBe(0);
 
-    addItem(app, JSON.stringify({ typ: EType.CARD, id: id0, tit: "Hello, World" }));
-    addItem(app, JSON.stringify({ typ: EType.CARD, id: id1, tit: "Hello, World" }));
-    addItem(app, JSON.stringify({ typ: EType.CARD, id: id2, tit: "Hello, World" }));
+    add(app, JSON.stringify({ typ: EType.CARD, id: id0, tit: "Hello, World", x: 10, y: 10, w: 50, h:30, col: "#000", bgcol: "#ec9" }));
+    add(app, JSON.stringify({ typ: EType.CARD, id: id1, tit: "Hello, World" }));
+    add(app, JSON.stringify({ typ: EType.CARD, id: id2, tit: "Hello, World" }));
     expect(app.state.size).toBe(3);
     expect(app.undo.length).toBe(3);
     expect(app.redo.length).toBe(0);
@@ -45,7 +44,7 @@ test('Math.sqrt()', () => {
     expect(app.undo[2].item1).toBe("{\"typ\":1,\"id\":\"836ca1f3-5956-41a8-9044-3f19444b5f3d\",\"tit\":\"Hello, World\"}");
     expect(app.undo[2].item2).toBe("{}");
 
-    removeItem(app, JSON.stringify({ typ: EType.CARD, id: id1, tit: "Hello, World" }));
+    remove(app, JSON.stringify({ typ: EType.CARD, id: id1, tit: "Hello, World" }));
     expect(app.state.size).toBe(2);
     expect(app.undo.length).toBe(4);
     expect(app.redo.length).toBe(0);
@@ -61,7 +60,9 @@ test('Math.sqrt()', () => {
     expect(app.undo.length).toBe(3);
     expect(app.redo.length).toBe(1);
 
-    addItem(app, JSON.stringify({ typ: EType.CARD, id: id4, tit: "Hello, World" }));
+    const json1 = { typ: EType.CARD, id: id3, tit: "Hello, World" };
+    const jsonStr1 = JSON.stringify(json1);
+    add(app, jsonStr1);
     expect(app.state.size).toBe(4);
     expect(app.undo.length).toBe(4);
     expect(app.redo.length).toBe(0);
@@ -76,6 +77,26 @@ test('Math.sqrt()', () => {
     expect(app.redo[0].op).toBe(1);
     expect(app.redo[0].item1).toBe("{\"typ\":1,\"id\":\"1f75218b-39c8-46e4-956b-edbed0235379\",\"tit\":\"Hello, World\"}");
     expect(app.redo[0].item2).toBe("{}");
+
+    add(app, jsonStr1);
+    expect(app.state.size).toBe(1);
+    expect(app.undo.length).toBe(1);
+    expect(app.redo.length).toBe(0);
+
+    const json2 = { typ: EType.CARD, id: id3, tit: "Welcome, World" }
+    const jsonStr2 = JSON.stringify(json2);
+    expect(app.state.get(id3)).toBe("{\"typ\":1,\"id\":\"1f75218b-39c8-46e4-956b-edbed0235379\",\"tit\":\"Hello, World\"}");
+    change(app, jsonStr1, jsonStr2);
+    expect(app.state.get(id3)).toBe("{\"typ\":1,\"id\":\"1f75218b-39c8-46e4-956b-edbed0235379\",\"tit\":\"Welcome, World\"}");
+    
+    undo(app);
+    expect(app.state.get(id3)).toBe("{\"typ\":1,\"id\":\"1f75218b-39c8-46e4-956b-edbed0235379\",\"tit\":\"Hello, World\"}");
+
+    redo(app);
+    expect(app.state.get(id3)).toBe("{\"typ\":1,\"id\":\"1f75218b-39c8-46e4-956b-edbed0235379\",\"tit\":\"Welcome, World\"}");
+
+    undo(app);
+    expect(app.state.get(id3)).toBe("{\"typ\":1,\"id\":\"1f75218b-39c8-46e4-956b-edbed0235379\",\"tit\":\"Hello, World\"}");
 });
 
 
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
-
+import './style.css'
 import { v4 as uuidv4 } from 'uuid';
 
 export enum EType { ERR = 0, CARD, CONN, FRAME, SVG };
 export enum EOp { ERR = 0, ADD, REMOVE, CHANGE, CLEARALL, UNDO, REDO };
 export type TJsonString = string;
 
-export interface IItem { typ: EType; id: string; };
-export interface ICard { typ: EType; id: string; tit: string; }
+export interface IItem {
+	typ: EType; id: string;
+};
+export interface ICard {
+	typ: EType; id: string; tit: string;
+	x: number; y: number; w: number; h: number;
+	col: string; bgcol: string;
+}
 export interface ICmd { op: EOp, item1: TJsonString, item2: TJsonString };
 export interface IApp {
 	state: Map<string, TJsonString>;
@@ -19,17 +25,24 @@ function pushTo(arr: Array<ICmd>, item1: TJsonString, item2: TJsonString, op: EO
 	arr.push(cmd);
 }
 
-export function addItem(app: IApp, item: TJsonString) {
+export function add(app: IApp, item: TJsonString) {
 	const json: IItem = JSON.parse(item as string) as IItem;
 	app.state.set(json.id, item);
 	pushTo(app.undo, item, "{}", EOp.REMOVE);
 	app.redo.length = 0;
 }
 
-export function removeItem(app: IApp, item: TJsonString) {
+export function remove(app: IApp, item: TJsonString) {
 	const json: IItem = JSON.parse(item as string) as IItem;
 	app.state.delete(json.id);
 	pushTo(app.undo, item, "{}", EOp.ADD);
+	app.redo.length = 0;
+}
+
+export function change(app: IApp, item1: TJsonString, item2: TJsonString) {
+	const json1: IItem = JSON.parse(item1 as string) as IItem;
+	app.state.set(json1.id, item2);
+	pushTo(app.undo, item1, item2, EOp.CHANGE);
 	app.redo.length = 0;
 }
 
@@ -48,6 +61,16 @@ export function undo(app: IApp) {
 			pushTo(app.redo, cmd.item1, "{}", EOp.ADD);
 			break;
 		}
+
+		case EOp.CHANGE: {
+			const json1: IItem = JSON.parse(cmd.item1 as string) as IItem;
+			app.state.set(json1.id, cmd.item1);
+			pushTo(app.redo, cmd.item2, cmd.item1, EOp.CHANGE);
+			break;
+		}
+
+		default:
+			throw "Unknown operation: " + cmd.op;
 	}
 };
 
@@ -66,16 +89,30 @@ export function redo(app: IApp) {
 			pushTo(app.undo, cmd.item1, "{}", EOp.ADD);
 			break;
 		}
+		case EOp.CHANGE: {
+			const json1: IItem = JSON.parse(cmd.item1 as string) as IItem;
+			app.state.set(json1.id, cmd.item1);
+			pushTo(app.undo, cmd.item2, cmd.item1, EOp.CHANGE);
+			break;
+		}
+
+		default:
+			throw "Unknown operation: " + cmd.op;
 	}
 };
 
-// function render(app: any) {
-// 	// dots.innerHTML = '';
-// 	const out: Array<string> = [];
-// 	app.history[app.position].forEach(function (item: any) {
-// 		out.push(item);
-// 	});
-// }
+function render(app: IApp) {
+	let innerHTML = "";
+	app.state.forEach((value: string, key: string) => {
+		const json: ICard = JSON.parse(value as string) as ICard;
+		innerHTML += `<div id="${json.id}" class="card" style="translate: ${json.x}px ${json.y}px; background-color: hsl(${json.bgcol} 99% 90%);" contenteditable="true" >${json.tit}</div>`;
+	});
+	const mapDiv: HTMLDivElement = document.getElementById("map") as HTMLDivElement;
+	mapDiv.innerHTML = innerHTML;
+}
+
+
+
 
 function startTime() {
 	return performance.now();
@@ -109,58 +146,125 @@ function printApp(app: IApp) {
 		}
 	});
 }
-function main() {
-
-	const app : IApp = {
+function runAddRemoveTest() {
+	const app: IApp = {
 		state: new Map<string, TJsonString>(),
 		undo: new Array<ICmd>(),
 		redo: new Array<ICmd>(),
 	}
-	
+
+	if (0) {
+		const start = startTime();
+		console.log("============== elapsed time: start ==============");
+		const N = 1000000;
+		for (let i = 0; i < N; i++) {
+			add(app, JSON.stringify({ typ: EType.CARD, id: uuidv4(), tit: "Hello, World" }));
+		}
+		const elapsed = endTime(start);
+		console.log("============== elapsed time: " + elapsed + " msecs ==============");
+	}
+	else {
+		console.log(`==== OPERATION 3 x ADD  ====`);
+		add(app, JSON.stringify({ typ: EType.CARD, id: (0).toString(), tit: "Card 1", x: 10, y: 10, w: 50, h: 30, col: "#000", bgcol: "#ec9" }));
+		add(app, JSON.stringify({ typ: EType.CARD, id: (1).toString(), tit: "Card 2", x: 60, y: 90, w: 50, h: 30, col: "#000", bgcol: "#ec9" }));
+		add(app, JSON.stringify({ typ: EType.CARD, id: (2).toString(), tit: "Card with a long title", x: 60, y: 170, w: 50, h: 30, col: "#000", bgcol: "#ec9" }));
+		printApp(app);
+
+		const toRemove = 1;
+		console.log(`==== OPERATION REMOVE ${toRemove} ====`);
+		remove(app, JSON.stringify({ typ: EType.CARD, id: (toRemove).toString(), tit: "Hello, World" }));
+		printApp(app);
+
+		console.log(`==== OPERATION UNDO ====`);
+		undo(app);
+		printApp(app);
+
+		console.log(`==== OPERATION UNDO ====`);
+		undo(app);
+		printApp(app);
+
+		console.log(`==== OPERATION REDO ====`);
+		redo(app);
+		printApp(app);
+
+		const toInsert = 4;
+		console.log(`==== OPERATION ADD ${toInsert} ====`);
+		// addItem(appState, JSON.stringify({ typ: EType.CARD, id: uuidv4(), tit: "Hello, World" }));
+		add(app, JSON.stringify({ typ: EType.CARD, id: (toInsert).toString(), tit: "Hello, World" }));
+		printApp(app);
+
+		console.log(`==== OPERATION 4x UNDO ====`);
+		undo(app);
+		undo(app);
+		undo(app);
+		undo(app);
+		printApp(app);
+
+		console.log(`==== OPERATION ADD uuid ====`);
+		const id5 = uuidv4();
+		const json1 = { typ: EType.CARD, id: id5, tit: "Hello, World" };
+		const jsonStr1 = JSON.stringify(json1);
+		const json2 = { typ: EType.CARD, id: id5, tit: "Brave Universe" };
+		const jsonStr2 = JSON.stringify(json2);
+		add(app, jsonStr1);
+		printApp(app);
+
+		console.log(`==== OPERATION CHANGE uuid ====`);
+		change(app, jsonStr1, jsonStr2);
+		printApp(app);
+
+		console.log(`==== OPERATION UNDO uuid ====`);
+		undo(app);
+		printApp(app);
+
+		console.log(`==== OPERATION REDO uuid ====`);
+		redo(app);
+		printApp(app);
+
+		console.log(`==== OPERATION UNDO uuid ====`);
+		undo(app);
+		printApp(app);
+	}
+
+}
+
+function runRenderTest() {
+	const app: IApp = {
+		state: new Map<string, TJsonString>(),
+		undo: new Array<ICmd>(),
+		redo: new Array<ICmd>(),
+	}
+
+	add(app, JSON.stringify({ typ: EType.CARD, id: (0).toString(), tit: "Card 1", x: 10, y: 10, w: 50, h: 30, col: "#000", bgcol: .3 }));
+	add(app, JSON.stringify({ typ: EType.CARD, id: (1).toString(), tit: "Card 2", x: 60, y: 90, w: 50, h: 30, col: "#000", bgcol: .5 }));
+	add(app, JSON.stringify({ typ: EType.CARD, id: (2).toString(), tit: "Card with a long title", x: 60, y: 170, w: 50, h: 30, col: "#000", bgcol: .6 }));
+
+	render(app);
+}
+
+function rndInt(min: number, max: number) { // min and max included 
+	return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+  
+function runRenderBigTest() {
+	const app: IApp = {
+		state: new Map<string, TJsonString>(),
+		undo: new Array<ICmd>(),
+		redo: new Array<ICmd>(),
+	}
 	const start = startTime();
 	console.log("============== elapsed time: start ==============");
-	const N = 5;
+	const N = 100;
 	for (let i = 0; i < N; i++) {
-		addItem(app, JSON.stringify({ typ: EType.CARD, id: uuidv4(), tit: "Hello, World" }));
+		add(app, JSON.stringify({ typ: EType.CARD, id: uuidv4(), tit: "Hello, World", 
+		x: rndInt(0, 1000), y: rndInt(0, 800), bgcol: rndInt(0, 360) }));
 	}
+
+	render(app);
+
 	const elapsed = endTime(start);
 	console.log("============== elapsed time: " + elapsed + " msecs ==============");
-
-
-	console.log(`==== OPERATION 3 x ADD  ====`);
-	addItem(app, JSON.stringify({ typ: EType.CARD, id: (0).toString(), tit: "Hello, World" }));
-	addItem(app, JSON.stringify({ typ: EType.CARD, id: (1).toString(), tit: "Hello, World" }));
-	addItem(app, JSON.stringify({ typ: EType.CARD, id: (2).toString(), tit: "Hello, World" }));
-	printApp(app);
-
-	const toRemove = 1;
-	console.log(`==== OPERATION REMOVE ${ toRemove } ====`);
-	removeItem(app, JSON.stringify({ typ: EType.CARD, id: (toRemove).toString(), tit: "Hello, World" }));
-	printApp(app);
-	
-	console.log(`==== OPERATION UNDO ====`);
-	undo(app);
-	printApp(app);
-	
-	console.log(`==== OPERATION UNDO ====`);
-	undo(app);
-	printApp(app);
-	
-	console.log(`==== OPERATION REDO ====`);
-	redo(app);
-	printApp(app);
-	
-	const toInsert = 4;
-	console.log(`==== OPERATION ADD ${ toInsert } ====`);
-	// addItem(appState, JSON.stringify({ typ: EType.CARD, id: uuidv4(), tit: "Hello, World" }));
-	addItem(app, JSON.stringify({ typ: EType.CARD, id: (toInsert).toString(), tit: "Hello, World" }));
-	printApp(app);
-	
-	console.log(`==== OPERATION 4x UNDO ====`);
-	undo(app);
-	undo(app);
-	undo(app);
-	undo(app);
-	printApp(app);	
-}	
-main();
+}
+// runAddRemoveTest();
+// runRenderTest();
+// runRenderBigTest();
