bd8677548732ae13139ae15fe6e6067c92e4885e
namapu <namapu@namapu.com>
2023-03-12 12:50:10 +0100

    add remove undo redo with unit tests

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,69 +1,81 @@
 // example.test.js
 import { expect, test } from 'vitest';
-import Immutable from 'immutable';
-import { ItemType, addCard, removeItem, undo, redo } from './main';
+import { v4 as uuidv4 } from 'uuid';
+
+import { IApp, TJsonString, ICmd, EType, addItem, removeItem, undo, redo } from './main';
 
 test('Math.sqrt()', () => {
-    const appState = {
-        history: [Immutable.List([])],
-        index: 0,
+
+    // a0	S:0 		U:r0 			R:          Length: 1 1 0 
+    // a1	S:0-1 		U:r0-r0 		R:          Length: 2 2 0 
+    // a2	S:0-1-2 	U:r0-r1-r2 		R:          Length: 3 3 0 
+    // r1	S:0-2		U:r0-r1-r2-a1	R:          Length: 2 4 0 
+    // u	S:0-2-1		U:r0-r1-r2		R:r1        Length: 3 3 1 
+    // u	S:0-1		U:r0-r1			R:a1-a2     Length: 2 2 2 
+    // r	S:0-1-2		U:r0-r1-r2		R:a1        Length: 3 3 1 
+    // a4   S:0-1-2-4   U:r0-r1-r2-r4   R:          Length: 3 3 0
+    // u
+    // u
+    // u
+    // u    S:          U:              R:a4-a2-a1-a0   Length: 0 0 4
+    const app: IApp = {
+        state: new Map<string, TJsonString>(),
+        undo: new Array<ICmd>(),
+        redo: new Array<ICmd>(),
     }
-    expect(appState.index).toBe(0);
-    expect(appState.history[0]).toBe(Immutable.List());
 
-    addCard(appState, "1", "hello1", 10, 10, 30, 30);
-    expect(appState.index).toBe(1);
-    expect(appState.history[1].toJS()).toStrictEqual([
-        { id: "1", type: ItemType.CARD, label: "hello1", x: 10, y: 10, w: 30, h: 30 }]);
+    const id0 = "6eac60fe-79f1-4f93-8566-3cbb348e0912"; //uuidv4();
+    const id1 = "68ea219a-d092-431b-8393-b94fe8e2c889"; //uuidv4();
+    const id2 = "836ca1f3-5956-41a8-9044-3f19444b5f3d"; //uuidv4();
+    const id3 = "e1f292fe-866c-42a0-a45f-d80f46e3c35f"; //uuidv4();
+    const id4 = "1f75218b-39c8-46e4-956b-edbed0235379"; //uuidv4();
 
-    addCard(appState, "2", "hello2", 10, 10, 30, 30);
-    expect(appState.index).toBe(2);
-    expect(appState.history[1].toJS()).toStrictEqual([
-        { id: "1", type: ItemType.CARD, label: "hello1", x: 10, y: 10, w: 30, h: 30 }]);
-    expect(appState.history[2].toJS()).toStrictEqual([
-        { id: "1", type: ItemType.CARD, label: "hello1", x: 10, y: 10, w: 30, h: 30 },
-        { id: "2", type: ItemType.CARD, label: "hello2", x: 10, y: 10, w: 30, h: 30 }]);
+    expect(app.state.size).toBe(0);
+    expect(app.undo.length).toBe(0);
+    expect(app.redo.length).toBe(0);
 
-    addCard(appState, "3", "hello2", 10, 10, 30, 30);
-    expect(appState.index).toBe(3);
-    addCard(appState, "4", "hello2", 10, 10, 30, 30);
-    expect(appState.index).toBe(4);
-    expect(appState.history[4].toJS()).toStrictEqual([
-        { id: "1", type: ItemType.CARD, label: "hello1", x: 10, y: 10, w: 30, h: 30 },
-        { id: "2", type: ItemType.CARD, label: "hello2", x: 10, y: 10, w: 30, h: 30 },
-        { id: "3", type: ItemType.CARD, label: "hello2", x: 10, y: 10, w: 30, h: 30 },
-        { id: "4", type: ItemType.CARD, label: "hello2", x: 10, y: 10, w: 30, h: 30 }]);
+    addItem(app, JSON.stringify({ typ: EType.CARD, id: id0, tit: "Hello, World" }));
+    addItem(app, JSON.stringify({ typ: EType.CARD, id: id1, tit: "Hello, World" }));
+    addItem(app, JSON.stringify({ typ: EType.CARD, id: id2, tit: "Hello, World" }));
+    expect(app.state.size).toBe(3);
+    expect(app.undo.length).toBe(3);
+    expect(app.redo.length).toBe(0);
+    expect(app.state.get(id2)).toBe("{\"typ\":1,\"id\":\"836ca1f3-5956-41a8-9044-3f19444b5f3d\",\"tit\":\"Hello, World\"}");
+    expect(app.undo[2].op).toBe(2);
+    expect(app.undo[2].item1).toBe("{\"typ\":1,\"id\":\"836ca1f3-5956-41a8-9044-3f19444b5f3d\",\"tit\":\"Hello, World\"}");
+    expect(app.undo[2].item2).toBe("{}");
 
-    removeItem(appState, "2");
-    expect(appState.index).toBe(5);
-    expect(appState.history[5].toJS()).toStrictEqual([
-        { id: "1", type: ItemType.CARD, label: "hello1", x: 10, y: 10, w: 30, h: 30 },
-        { id: "3", type: ItemType.CARD, label: "hello2", x: 10, y: 10, w: 30, h: 30 },
-        { id: "4", type: ItemType.CARD, label: "hello2", x: 10, y: 10, w: 30, h: 30 }]);
+    removeItem(app, JSON.stringify({ typ: EType.CARD, id: id1, tit: "Hello, World" }));
+    expect(app.state.size).toBe(2);
+    expect(app.undo.length).toBe(4);
+    expect(app.redo.length).toBe(0);
 
-    addCard(appState, "5", "hello5", 10, 10, 30, 30);
-    expect(appState.index).toBe(6);
-    expect(appState.history[6].toJS()).toStrictEqual([
-        { id: "1", type: ItemType.CARD, label: "hello1", x: 10, y: 10, w: 30, h: 30 },
-        { id: "3", type: ItemType.CARD, label: "hello2", x: 10, y: 10, w: 30, h: 30 },
-        { id: "4", type: ItemType.CARD, label: "hello2", x: 10, y: 10, w: 30, h: 30 },
-        { id: "5", type: ItemType.CARD, label: "hello5", x: 10, y: 10, w: 30, h: 30 }]);
-   
-    undo(appState);
-    undo(appState);
-    expect(appState.index).toBe(4);
-    expect(appState.history[4].toJS()).toStrictEqual([
-        { id: "1", type: ItemType.CARD, label: "hello1", x: 10, y: 10, w: 30, h: 30 },
-        { id: "2", type: ItemType.CARD, label: "hello2", x: 10, y: 10, w: 30, h: 30 },
-        { id: "3", type: ItemType.CARD, label: "hello2", x: 10, y: 10, w: 30, h: 30 },
-        { id: "4", type: ItemType.CARD, label: "hello2", x: 10, y: 10, w: 30, h: 30 }]);
-    
-    redo(appState);
-    expect(appState.index).toBe(5);
-    expect(appState.history[5].toJS()).toStrictEqual([
-        { id: "1", type: ItemType.CARD, label: "hello1", x: 10, y: 10, w: 30, h: 30 },
-        { id: "3", type: ItemType.CARD, label: "hello2", x: 10, y: 10, w: 30, h: 30 },
-        { id: "4", type: ItemType.CARD, label: "hello2", x: 10, y: 10, w: 30, h: 30 }]);
+    undo(app);
+    undo(app);
+    expect(app.state.size).toBe(2);
+    expect(app.undo.length).toBe(2);
+    expect(app.redo.length).toBe(2);
+
+    redo(app);
+    expect(app.state.size).toBe(3);
+    expect(app.undo.length).toBe(3);
+    expect(app.redo.length).toBe(1);
+
+    addItem(app, JSON.stringify({ typ: EType.CARD, id: id4, tit: "Hello, World" }));
+    expect(app.state.size).toBe(4);
+    expect(app.undo.length).toBe(4);
+    expect(app.redo.length).toBe(0);
+
+    undo(app);
+    undo(app);
+    undo(app);
+    undo(app);
+    expect(app.state.size).toBe(0);
+    expect(app.undo.length).toBe(0);
+    expect(app.redo.length).toBe(4);
+    expect(app.redo[0].op).toBe(1);
+    expect(app.redo[0].item1).toBe("{\"typ\":1,\"id\":\"1f75218b-39c8-46e4-956b-edbed0235379\",\"tit\":\"Hello, World\"}");
+    expect(app.redo[0].item2).toBe("{}");
 });
 
 
--- src/main.ts
+++ src/main.ts
@@ -1,343 +1,166 @@
-import Immutable from 'immutable';
-import {v4 as uuidv4} from 'uuid';
 
-export enum ItemType {
-    UNDEF = 0,
-    CARD,
-    CONN,
-    FRAME,
+import { v4 as uuidv4 } from 'uuid';
+
+export enum EType { ERR = 0, CARD, CONN, FRAME, SVG };
+export enum EOp { ERR = 0, ADD, REMOVE, CHANGE, CLEARALL, UNDO, REDO };
+export type TJsonString = string;
+
+export interface IItem { typ: EType; id: string; };
+export interface ICard { typ: EType; id: string; tit: string; }
+export interface ICmd { op: EOp, item1: TJsonString, item2: TJsonString };
+export interface IApp {
+	state: Map<string, TJsonString>;
+	undo: Array<ICmd>;
+	redo: Array<ICmd>;
 };
 
-const appState = {
-    history: Immutable.List([Immutable.List([])]),
-    position: 0,
+function pushTo(arr: Array<ICmd>, item1: TJsonString, item2: TJsonString, op: EOp) {
+	const cmd: ICmd = { op: op, item1: item1, item2: item2 };
+	arr.push(cmd);
 }
 
-function operation(apst: any, fn: any) {
-    apst.history = apst.history.slice(0, apst.position + 1);
-    var newVersion = fn(apst.history[apst.position]);
-    apst.history.push(newVersion);
-    apst.position++;
-    // draw();
+export function addItem(app: IApp, item: TJsonString) {
+	const json: IItem = JSON.parse(item as string) as IItem;
+	app.state.set(json.id, item);
+	pushTo(app.undo, item, "{}", EOp.REMOVE);
+	app.redo.length = 0;
 }
 
-// here are our two operations: addDot is what
-// you trigger by clicking the blank
-function addItem(apst: any, item: any) {
-    operation(apst, function (data: any) {
-        //const item = Immutable.Map({ id: id, title: "hello", x: 10, y: 10, w: 30, h: 30 });
-        return data.push(Immutable.Map(item));
-    });
-    // console.log("add: ", item);
+export function removeItem(app: IApp, item: TJsonString) {
+	const json: IItem = JSON.parse(item as string) as IItem;
+	app.state.delete(json.id);
+	pushTo(app.undo, item, "{}", EOp.ADD);
+	app.redo.length = 0;
 }
 
-export function addCard(apst: any, id: string, title: string, x: number, y: number, w: number, h: number) {
-    addItem(apst, { id: id, type: ItemType.CARD, title: title, x: x, y: y, w: w, h: h });
-}
-
-export function addConn(apst: any, id: string, title: string, x: number, y: number, w: number, h: number) {
-    addItem(apst, { id: id, type: ItemType.CONN, title: title, x: x, y: y, w: w, h: h });
-}
-
-export function addFrame(apst: any, id: string, title: string, x: number, y: number, w: number, h: number) {
-    addItem(apst, { id: id, type: ItemType.FRAME, title: title, x: x, y: y, w: w, h: h });
-}
-
-
-export function removeItem(appst: any, id: string) {
-    operation(appst, function (data: any) {
-        return data.filter(function (item: any) {
-            return item.get('id') !== id;
-        });
-    });
-    // console.log("delete: ", id);
-}
-
-export function undo(apst: any) {
-    if (apst.position > 0)
-        apst.position--;
-    // console.log("Undo");
+export function undo(app: IApp) {
+	const cmd: ICmd = app.undo.pop() as ICmd;
+	switch (cmd.op) {
+		case EOp.ADD: {
+			const json1: IItem = JSON.parse(cmd.item1 as string) as IItem;
+			app.state.set(json1.id, cmd.item1);
+			pushTo(app.redo, cmd.item1, "{}", EOp.REMOVE);
+			break;
+		}
+		case EOp.REMOVE: {
+			const json1: IItem = JSON.parse(cmd.item1 as string) as IItem;
+			app.state.delete(json1.id);
+			pushTo(app.redo, cmd.item1, "{}", EOp.ADD);
+			break;
+		}
+	}
 };
 
-export function redo(apst: any) {
-    if (apst.position < apst.history.length)
-        apst.position++;
-    // console.log("Redo");
+export function redo(app: IApp) {
+	const cmd: ICmd = app.redo.pop() as ICmd;
+	switch (cmd.op) {
+		case EOp.ADD: {
+			const json1: IItem = JSON.parse(cmd.item1 as string) as IItem;
+			app.state.set(json1.id, cmd.item1);
+			pushTo(app.undo, cmd.item1, "{}", EOp.REMOVE);
+			break;
+		}
+		case EOp.REMOVE: {
+			const json1: IItem = JSON.parse(cmd.item1 as string) as IItem;
+			app.state.set(json1.id, cmd.item1);
+			pushTo(app.undo, cmd.item1, "{}", EOp.ADD);
+			break;
+		}
+	}
 };
 
-function render(apst: any) {
-    // dots.innerHTML = '';
-    const out: Array<string> = [];
-    apst.history[apst.position].forEach(function (item: any) {
-        out.push(item);
-    });
-    // console.log(out.toString());
-    // undo.disabled = (historyposition != 0) ? '' : 'disabled';
-    // redo.disabled = (historyposition !== myHistory.length - 1) ? '' : 'disabled';
-}
-
-console.log("elapsed time: start");
-const N = 10;
-var startTime = performance.now();
-    // for (let i = 1; i < N; i++)
-    // {
-    //     addCard(appState2, Number(i).toString(), "hello1", 10, 10, 30, 30);
-    //     // console.log("added card");
-    // }
-    // for (let i = N-1; i > 0; i--)
-    // {
-    //     // removeItem(appState2, Number(i).toString());
-    // }
-    // let elapsed = new Date().getTime() - start;
-    type Item = {
-      id: string;
-      type: ItemType;
-      title: string;
-    }
-    let map = new Map<string, Item>();
-    for (let i = 0; i < N; i++) {
-      const uuid = uuidv4();
-      map.set(uuid.toString(), {id: uuid,  type: ItemType.CARD, title: "hello"});
-    }
-
-    console.log("map size: ", map.size);
-
-    map.forEach((value: Item, key: string) => {
-      console.log(key, value);
-    });    
-
-    map.forEach((value: Item, key: string) => {
-      value;
-      map.delete(key);
-    });    
-
-    for (let i = N - 1; i >= 0; i--) {
-      map.delete(i.toString());
-    }
-
-    console.log("map size: ", map.size);
-    
-    var endTime = performance.now();
-    let elapsed = endTime - startTime;
-    console.log("map size: ", map.size);
-    console.log("elapsed time: ", elapsed, "msecs");
-
-
-///====================
-
-// addCard(appState, "1", "hello", 10, 10, 30, 30);
-// render(appState);
-
-// addItem(appState, { id: "2", type: ItemType.CARD, title: "hello", x: 10, y: 10, w: 30, h: 30 });
-// addItem(appState, { id: "3", type: ItemType.CARD, title: "hello", x: 10, y: 10, w: 30, h: 30 });
-// addItem(appState, { id: "4", type: ItemType.CARD, title: "hello", x: 10, y: 10, w: 30, h: 30 });
-// render(appState);
-
-// removeItem(appState, "2");
-// render(appState);
-
-// addItem(appState, { id: "5", type: ItemType.CARD, title: "hello", x: 10, y: 10, w: 30, h: 30 });
-// render(appState);
-
-// undo(appState);
-// undo(appState);
-// render(appState);
-
-// redo(appState);
-// render(appState);
-
-/*
-
-function renderCard(id: number) {
-  // if (cardsDiv === null) return;
-  // if (undoButton === null) return;
-  // if (redoButton === null) return;
-
-  // cardsDiv.innerHTML = '';
-  const states = appState.history.states;
-  const position = appState.history.position;
-  const state : TState = states[position] as TState;
-  const cards = state.cards;
-  state.forEach(function(card) {
-      const c: TCard = state.cards;
-      if (card.id === id) {
-        console.log("card: " + id);
-        // var elem = cardsDiv.appendChild(document.createElement('div'));
-        // setCard(elem, card);
-      }
-  });
-  // undoButton.disabled = (history.position != 0) ? false : true;
-  // redoButton.disabled = (history.position !== history.states.length - 1) ? false : true;
-}
-*/
-
-/*
-import App from './app';
-import { TAppState, THistory, UiState as TUiState } from './types';
-import Immutable from 'immutable';
-
-const uiState: TUiState = { };
-const history: THistory = { states: [Immutable.List([])], position: 0 };
-const appState: TAppState = { history, uiState }
-
-
-let cardsDiv : HTMLDivElement;
-let undoButton : HTMLButtonElement; 
-let redoButton : HTMLButtonElement; 
-let loadButton : HTMLButtonElement; 
-let saveButton : HTMLButtonElement; 
-let newButton : HTMLButtonElement; 
-
-window.onload = () => {
-  const appElem = document.querySelector<HTMLDivElement>('#app') as HTMLDivElement;
-  const navElem = document.querySelector<HTMLDivElement>('#nav') as HTMLDivElement;
-  let app = new App(appState, appElem, navElem);
-  app.render();
-
-  cardsDiv = appElem; //document.getElementById('app') as HTMLButtonElement;
-  undoButton = document.getElementById('btn-undo') as HTMLButtonElement;
-  redoButton = document.getElementById('btn-redo') as HTMLButtonElement;
-  loadButton = document.getElementById('btn-load') as HTMLButtonElement;
-  saveButton = document.getElementById('btn-save') as HTMLButtonElement;
-  newButton = document.getElementById('btn-new') as HTMLButtonElement;
-
-  cardsDiv.addEventListener('dblclick', function(e) {
-    const title = "";
-    const id = +new Date();
-    addCard(e.pageX, e.pageY, color, title, id);
-    // color = color + 20;
-    // setFocus(elCard);
-  });
-  undoButton.addEventListener('click', function() {
-      if (history.position > 0) history.position--;
-      render();
-  });
-  redoButton.addEventListener('click', function() {
-      if (history.position < history.states.length) history.position++;
-      render();
-  });
-  saveButton.addEventListener('click', function() {
-    const immutable = history.states[history.position];
-    const obj = immutable.toJS(); 
-    const json = JSON.stringify(obj);
-    saveDoc('test', json);
-    // console.log(json);
-  });
-  loadButton.addEventListener('click', function() {
-    const json = loadDoc('test');
-    console.log(json);
-    if (json === null) return null;
-    const obj = JSON.parse(json);
-    // console.log(obj);
-    clearHistory();
-    history.states[history.position] = Immutable.fromJS(obj);
-    render();
-    return true;
-  });
-  newButton.addEventListener('click', function() {
-    clearHistory();
-    render();
-  });
-
-  render();
-  }
-
-function operation(fn: any) {
-  // first, make sure that there is no future in the history list. For instance, if the user renders something, 
-  // clicks undo, and then renders something else, we need to dispose of the future state
-  const position = history.position;
-  history.states = history.states.slice(0, position + 1);
-  const newVersion = fn(history.states[position]); // create a new version of the data by applying a given function to the current head
-  history.states.push(newVersion); // add the new version to the history list and increment the position to match
-  history.position = position + 1;
-
-  render();
-}
-
-function addCard(x: number, y: number, color: number, name: string, id: number) {
-  operation(function(data: any) {
-      return data.push(Immutable.Map({
-          x: x, y: y, color: color, name: name, id: id
-      }));
-  });
-}
-
-// function removeCard(id: string) {
-//   operation(function(data: any) {
-//       return data.filter(function(card: any) {
-//           return card.get('id') !== id;
-//       });
-//   });
+// function render(app: any) {
+// 	// dots.innerHTML = '';
+// 	const out: Array<string> = [];
+// 	app.history[app.position].forEach(function (item: any) {
+// 		out.push(item);
+// 	});
 // }
 
-const setCardLeftTop = (elem: any, card: any) => {
-  elem.style.left = card.get('x') + 'px';
-  elem.style.top = card.get('y') + 'px';
+function startTime() {
+	return performance.now();
 }
-
-const setCardColor = (elem: any, card: any) => {
-  // const color = card.get('color'); 
-  // elem.style.backgroundColor = `hsl(${color} 99% 80%`;
-  // elem.style.color = `hsl(${color} 99% 0%`;
+function endTime(start: number) {
+	return performance.now() - start;
 }
+function printApp(app: IApp) {
+	const PRINT_LIMIT = 5;
+	console.log("*** STATE: " + app.state.size + " items ***");
+	app.state.forEach((value: string, key: string) => {
+		if (PRINT_LIMIT < 6) {
+			// console.log(key, JSON.parse(value));
+			console.log(key, value);
+		}
+	});
 
-const setCardTitle = (elem: any, card: any) => {
-  elem.innerHTML = card.get('title');
+	console.log("*** UNDO: " + app.undo.length + " items ***");
+	app.undo.forEach((cmd: ICmd) => {
+		if (PRINT_LIMIT < 6) {
+			// console.log(cmd.op.toString() + ", " + cmd.item1 + ", " + cmd.item2);
+			console.log(cmd);
+		}
+	});
+
+	console.log("*** REDO: " + app.redo.length + " items ***");
+	app.redo.forEach((cmd: ICmd) => {
+		if (PRINT_LIMIT < 6) {
+			// console.log(cmd.op.toString() + ", " + cmd.item1 + ", " + cmd.item2);
+			console.log(cmd);
+		}
+	});
 }
+function main() {
 
-const setCard = (elem: any, card: any) => {
-  elem.className = 'card';
-  setCardLeftTop(elem, card);
-  setCardColor(elem, card);
-  setCardTitle(elem, card);
-  elem.id = card.get('id'); 
-  elem.contentEditable = true;
-  // elem.draggable = true;
-  
-  // clicking on a card removes it.
-  elem.addEventListener('click', function(e: any) {
-      // removeCard(card.get('id'));
-      e.stopPropagation();
-  });
-}
-
-function render() {
-    if (cardsDiv === null) return;
-    if (undoButton === null) return;
-    if (redoButton === null) return;
-
-    cardsDiv.innerHTML = '';
-    history.states[history.position].forEach(function(card) {
-        var elem = cardsDiv.appendChild(document.createElement('div'));
-        setCard(elem, card);
-    });
-    undoButton.disabled = (history.position != 0) ? false : true;
-    redoButton.disabled = (history.position !== history.states.length - 1) ? false : true;
-}
-
-let color = 0;
-
-// function selectElementContents(el: any) {
-//   var range = document.createRange();
-//   range.selectNodeContents(el);
-//   var sel = window.getSelection();
-//   if (sel === null) return;
-//   sel.removeAllRanges();
-//   sel.addRange(range);
-// }
+	const app : IApp = {
+		state: new Map<string, TJsonString>(),
+		undo: new Array<ICmd>(),
+		redo: new Array<ICmd>(),
+	}
+	
+	const start = startTime();
+	console.log("============== elapsed time: start ==============");
+	const N = 5;
+	for (let i = 0; i < N; i++) {
+		addItem(app, JSON.stringify({ typ: EType.CARD, id: uuidv4(), tit: "Hello, World" }));
+	}
+	const elapsed = endTime(start);
+	console.log("============== elapsed time: " + elapsed + " msecs ==============");
 
 
-const clearHistory = () => {
-  history.states = [Immutable.List([])];
-  history.position = 0;
-}
+	console.log(`==== OPERATION 3 x ADD  ====`);
+	addItem(app, JSON.stringify({ typ: EType.CARD, id: (0).toString(), tit: "Hello, World" }));
+	addItem(app, JSON.stringify({ typ: EType.CARD, id: (1).toString(), tit: "Hello, World" }));
+	addItem(app, JSON.stringify({ typ: EType.CARD, id: (2).toString(), tit: "Hello, World" }));
+	printApp(app);
 
-const saveDoc = (name: string, data: any) => {
-  localStorage.setItem(name, data);
-}
-
-const loadDoc = (name: string) => {
-   return localStorage.getItem(name);
-}
-
-*/
+	const toRemove = 1;
+	console.log(`==== OPERATION REMOVE ${ toRemove } ====`);
+	removeItem(app, JSON.stringify({ typ: EType.CARD, id: (toRemove).toString(), tit: "Hello, World" }));
+	printApp(app);
+	
+	console.log(`==== OPERATION UNDO ====`);
+	undo(app);
+	printApp(app);
+	
+	console.log(`==== OPERATION UNDO ====`);
+	undo(app);
+	printApp(app);
+	
+	console.log(`==== OPERATION REDO ====`);
+	redo(app);
+	printApp(app);
+	
+	const toInsert = 4;
+	console.log(`==== OPERATION ADD ${ toInsert } ====`);
+	// addItem(appState, JSON.stringify({ typ: EType.CARD, id: uuidv4(), tit: "Hello, World" }));
+	addItem(app, JSON.stringify({ typ: EType.CARD, id: (toInsert).toString(), tit: "Hello, World" }));
+	printApp(app);
+	
+	console.log(`==== OPERATION 4x UNDO ====`);
+	undo(app);
+	undo(app);
+	undo(app);
+	undo(app);
+	printApp(app);	
+}	
+main();
